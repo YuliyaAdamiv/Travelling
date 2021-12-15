3a6f42e2038443bf98dab8ca75198c4567809589
Yuliya <yulua03071992@gmail.com>
2021-12-15 11:32:53 +0200

    add store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe} from './redux/state';
-import {addPost, updateNewPostText} from './redux/state';
+import store from './redux/state';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />{' '}
     </React.StrictMode>,
     document.getElementById('root')
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- my-app/src/redux/state.jsx
+++ my-app/src/redux/state.jsx
@@ -1,75 +1,80 @@
-let rerenderEntireTree = () => {
-  console.log('State changes');
-};
-let state = {
-  profilePage: {
-    posts: [
-      {message: 'Hi, how are you?', like: '15'},
-      {message: 'Hello', like: '20'},
-      {message: 'Where do you live?', like: '11'},
-      {message: 'Yo! Yo! Yo!', like: '150'},
-    ],
-    newPostText: 'Hello',
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {message: 'Hi, how are you?', like: '15'},
+        {message: 'Hello', like: '20'},
+        {message: 'Where do you live?', like: '11'},
+        {message: 'Yo! Yo! Yo!', like: '150'},
+      ],
+      newPostText: 'Hello',
+    },
+    messagePage: {
+      dialog: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'},
+      ],
+      message: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo?'},
+        {id: 4, message: 'Yo!'},
+        {id: 5, message: 'Yo3'},
+        {id: 6, message: 'Yo%'},
+      ],
+    },
+    sidebarPage: {
+      friend: [
+        {id: 1, name: 'Sveta'},
+        {id: 2, name: 'Viktor'},
+        {id: 3, name: 'Sasha'},
+      ],
+      channel: [
+        {
+          name: 'Relaxing',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh0-2Vro_50sC5LwLG2cqqEFQOUGj11f9RaA&usqp=CAU',
+        },
+        {
+          name: 'Travelling',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUlDa7v2UlsDcBHBuzEzDwtQxrz3PA6DqeFw&usqp=CAU',
+        },
+        {
+          name: 'Route',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCFWLzxfuVQtvQZfE9s8OYOqMCHjUxNJujyg&usqp=CAU',
+        },
+      ],
+    },
   },
-  messagePage: {
-    dialog: [
-      {id: 1, name: 'Dimych'},
-      {id: 2, name: 'Andrey'},
-      {id: 3, name: 'Sveta'},
-      {id: 4, name: 'Sasha'},
-      {id: 5, name: 'Viktor'},
-      {id: 6, name: 'Valera'},
-    ],
-    message: [
-      {id: 1, message: 'Hi'},
-      {id: 2, message: 'How are you?'},
-      {id: 3, message: 'Yo?'},
-      {id: 4, message: 'Yo!'},
-      {id: 5, message: 'Yo3'},
-      {id: 6, message: 'Yo%'},
-    ],
+  getState() {
+    return this._state;
   },
-  sidebarPage: {
-    friend: [
-      {id: 1, name: 'Sveta'},
-      {id: 2, name: 'Viktor'},
-      {id: 3, name: 'Sasha'},
-    ],
-    channel: [
-      {
-        name: 'Relaxing',
-        src:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh0-2Vro_50sC5LwLG2cqqEFQOUGj11f9RaA&usqp=CAU',
-      },
-      {
-        name: 'Travelling',
-        src:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUlDa7v2UlsDcBHBuzEzDwtQxrz3PA6DqeFw&usqp=CAU',
-      },
-      {
-        name: 'Route',
-        src:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCFWLzxfuVQtvQZfE9s8OYOqMCHjUxNJujyg&usqp=CAU',
-      },
-    ],
+  _callSubscriber() {
+    console.log('State changes');
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      like: 7,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
-window.state = state;
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    like: 7,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-};
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-export default state;
+export default store;
+window.store = store;
